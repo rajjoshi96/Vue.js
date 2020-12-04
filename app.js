f381477a300c855c5f6715e1e828a669e0f967ca
Raj Joshi <43139255+rajjoshi96@users.noreply.github.com>
2020-12-05 00:20:04 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        title : 'Todo App',
+        newTodo:'',
+        todos:[]
+    },
+    methods: {
+        addTodo(){
+            this.todos.push({
+                title:this.newTodo,
+                done:false
+            });
+            this.newTodo='';
+        },
+        removeTodo(todo){
+            const todoIndex=this.todos.indexOf(todo);
+            this.todos.splice(todoIndex,1);
+        },
+        alldone(){
+            this.todos.forEach(todo => {todo.done=true;
+                
+            });
+                
+            ;
+        }
+    },
+});
